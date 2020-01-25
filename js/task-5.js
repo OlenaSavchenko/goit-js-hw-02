7519e47e4fa108dab77ace2c9ba385feffada619
Olena Savchenko <olsavchenko1@gmail.com>
2020-01-25 22:00:24 +0200

    for review

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+// const checkForSpam = function(message) {
+//   const normalizeMessage = message.toLowerCase().split(' ');
+
+//   return (
+//     normalizeMessage.includes('[spam]') || normalizeMessage.includes('sale')
+//   );
+// };
+
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
---
+++ js/task-6.js
@@ -0,0 +1,25 @@
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// while (true) {
+//   input = prompt('Введите число');
+
+//   if (input === null) {
+//     break;
+//   }
+//   input = Number(input);
+
+//   const notANumber = Number.isNaN(input);
+//   if (notANumber) {
+//     alert('Было введено не число, попробуйте еще раз');
+//     continue;
+//   }
+//   numbers.push(input);
+
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     total += numbers[i];
+//   }
+// }
+
+// console.log(`Общая сумма чисел равна ${total}`);
