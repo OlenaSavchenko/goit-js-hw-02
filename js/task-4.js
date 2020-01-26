b62b3093b7c0bb0f7d8e5c9c4d6f78a4083df3d5
Olena Savchenko <olsavchenko1@gmail.com>
2020-01-26 12:46:28 +0200

    changed task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,10 @@
 // const formatString = function(string) {
 //   if (string.length <= 40) {
 //     return string;
-//   } else if (string.length > 40) {
-//     return string.slice(0, 40) + '...';
 //   }
+//   return string.slice(0, 40) + '...';
 
-//   //   return string.length < 40 ? string : string.slice(0, 40) + '...';
+//   //   return string.length <= 40 ? string : string.slice(0, 40) + '...';
 // };
 
 // console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
